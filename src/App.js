32e8ed9f4b406f83a773cc70f5d717871ad29e2a
Anson Saver Dsouza <85802445+Ansonsd5@users.noreply.github.com>
2022-10-12 13:25:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
 import "./App.css";
-import logo from "./logo.png";
+import { ScrollToTop } from "./components/ScrollToTop/ScrollToTop";
+import { NavRoutes } from "./Routes/NavRoutes";
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        containerStyle={{
+          top: "4rem",
+        }}
+      />
+      <NavRoutes />
+      <ScrollToTop />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
+
+
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ProductProvider } from "./contexts/productContext";
+import { AuthProvider } from "./contexts/authContext";
+import { WishlistProvider } from "./contexts/wishlistContext";
+import { CartProvider } from "./contexts/cartContext";
+import { AddressProvider } from "./contexts/addressContext";
+import { OrderProvider } from "./contexts/orderContext";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <AuthProvider>
+        <ProductProvider>
+          <WishlistProvider>
+            <CartProvider>
+              <AddressProvider>
+                <OrderProvider>
+                  <App />
+                </OrderProvider>
+              </AddressProvider>
+            </CartProvider>
+          </WishlistProvider>
+        </ProductProvider>
+      </AuthProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
